e236deea4cc89afd9841e67b33a246e95223c65c
pablofc18 <pablofc18@gmail.com>
2025-01-25 17:38:08 +0100

    start spring boot

---
+++ src/test/java/com/vm1/appOkta/AppOktaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm1.appOkta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOktaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
