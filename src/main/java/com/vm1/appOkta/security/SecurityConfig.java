864b195770c8b73cc60e85fb260491646af2134d
pablofc18 <pablofc18@gmail.com>
2025-01-26 17:45:58 +0100

    m

--- src/main/java/com/vm1/appOkta/security/SecurityConfig.java
+++ src/main/java/com/vm1/appOkta/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.vm1.appOkta.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,7 +24,9 @@ public class SecurityConfig {
                 .redirectionEndpoint(endpoint -> endpoint
                     .baseUri("/login/oauth2/code/okta")
                 )   
-            );
+            )
+            .oauth2Client(Customizer.withDefaults());
+
 
         return http.build();
     }
