0b2988621322563f783d5b58e167a47118ccc544
pablofc18 <pablofc18@gmail.com>
2025-01-26 19:32:19 +0100

    nose a

--- src/main/java/com/vm1/appOkta/security/SecurityConfig.java
+++ src/main/java/com/vm1/appOkta/security/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.vm1.appOkta.security;
 
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,4 +44,28 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
+
+        // Configura un manejador de éxito personalizado
+        authorizedClientManager.setAuthorizationSuccessHandler(new OAuth2AuthorizationSuccessHandler() {
+            @Override
+            public void onAuthorizationSuccess(OAuth2AuthorizedClient authorizedClient,
+                                               Authentication principal,
+                                               Map<String, Object> attributes) {
+                // Establece el contexto de seguridad aquí
+                SecurityContextHolder.getContext().setAuthentication(principal);
+                System.out.println("Autenticación exitosa para el cliente: " + authorizedClient.getClientRegistration().getClientId());
+            }
+        });
+
+        return authorizedClientManager;
+    }
+
 }
