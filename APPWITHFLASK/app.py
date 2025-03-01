2c8f1a14a48cb6cea933657d1dca939c8863e421
pablofc18 <pablofc18@gmail.com>
2025-03-01 12:43:35 +0100

    import flash

--- APPWITHFLASK/app.py
+++ APPWITHFLASK/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, session, render_template, request
+from flask import Flask, redirect, url_for, session, render_template, request, flash
 from authlib.integrations.flask_client import OAuth
 from flask_sqlalchemy import SQLAlchemy
 from flask.json import jsonify
