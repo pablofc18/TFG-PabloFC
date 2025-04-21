63d4c4f02b206653bc1b6c9ed616c1455257e759
pablofc18 <pablofc18@gmail.com>
2025-04-21 18:56:24 +0200

    testing load data

--- connectors/entraid_utils.py
+++ connectors/entraid_utils.py
@@ -7,9 +7,80 @@ class EntraIDUtils:
     # Utility class to help interacting with Microsoft Entra ID (& Graph API)
     def __init__(self):
         load_dotenv("../env_vars.env")
-        self.tenant_id = os.getenv("TENANT_ID")
+        self.tenant_id = os.getenv("ENTRAID_TENANT_ID")
         self.graph_url = os.getenv("GRAPH_URL")
         self.login_microsoft_url = os.getenv("LOGIN_MICROSOFT_URL")
-        self.client_id = os.getenv("CLIENT_ID")
-        self.client_secret = os.getenv("CLIENT_SECRET")
+        self.client_id = os.getenv("ENTRAID_CLIENT_ID")
+        self.client_secret = os.getenv("ENTRAID_CLIENT_SECRET")
         self.token = None
+
+
+    # Get token for graph api
+    def get_token_graph(self) -> str:
+        url_graph = f"{self.login_microsoft_url}/{self.tenant_id}/oauth2/v2.0/token"
+        body = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "scope": f"{self.graph_url}/.default",
+            "grant_type": "client_credentials"
+        }
+        resp = requests.post(url_graph, data=body)
+        if resp.status_code != 200: # error
+            raise requests.HTTPError(
+                f"Error {resp.status_code} al cridar a {resp.url}: {resp.reason}"
+            )
+        resp_json = resp.json()
+        token = resp_json.get("access_token")
+        if not token:
+            raise ValueError("No access token found in response!!!")
+        
+        return token
+
+    # Headers for requests
+    def get_headers(self) -> dict:
+        if not self.token:
+            self.token = self.get_token_graph()
+        return {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json"
+        }
+
+    # Create users (multiple per 1 request)
+    def create_users_batch(self, user_payloads: list) -> dict:
+        batch_url = f"{self.graph_url}/v1.0/$batch"
+        requests_list = []
+        for idx, payload in enumerate(user_payloads):
+            requests_list.append({
+                "id": str(idx),
+                "method": "POST",
+                "url": "/users",
+                "headers": {"Content-Type": "application/json"},
+                "body": payload
+            })
+        batch_body = {"requests": requests_list}
+        resp = requests.post(batch_url, json=batch_body, headers=self.get_headers())
+        if resp.status_code != 200: # error
+            raise requests.HTTPError(
+                f"Error {resp.status_code} al cridar a {resp.url}: {resp.reason}"
+            )
+        return resp.json()
+
+    # Create groups (multple per 1 request)
+    def create_groups_batch(self, group_payloads: list) -> dict:
+        batch_url = f"{self.graph_url}/v1.0/$batch"
+        requests_list = []
+        for idx, payload in enumerate(group_payloads):
+            requests_list.append({
+                "id": str(idx),
+                "method": "POST",
+                "url": "/groups",
+                "headers": {"Content-Type": "application/json"},
+                "body": payload
+            })
+        batch_body = {"requests": requests_list}
+        resp = requests.post(batch_url, json=batch_body, headers=self.get_headers())
+        if resp.status_code != 200: # error
+            raise requests.HTTPError(
+                f"Error {resp.status_code} al cridar a {resp.url}: {resp.reason}"
+            )
+        return resp.json()
--- connectors/load_data.py
+++ connectors/load_data.py
@@ -1,11 +1,14 @@
 import os
+import json
 from dotenv import load_dotenv
 from cipher_utils import AESHelper
+from entraid_utils import EntraIDUtils
 
 
 class LoadEntraIdData:
-    def __init__(self, aes_key: bytes,):
+    def __init__(self, aes_key: bytes):
         self.aes_key = aes_key
+        self.entraid_utils = EntraIDUtils()
 
     # Retorna json
     def decrypt_file_to_json(self, file_enc_path: str):
@@ -13,8 +16,19 @@ class LoadEntraIdData:
         return decryptor.decrypt_file(file_enc_path)
 
     # Create users in Entra ID
-    def create_users(self,):
+    def create_users(self, users_enc_path: str, results_path: str):
+        users_json = self.decrypt_file_to_json(users_enc_path)
+        batch_response = self.entraid_utils.create_users_batch(users_json)
+        if results_path:
+            with open(results_path, "w", encoding="utf-8") as f:
+                json.dump(batch_response, f, ensure_ascii=False, indent=2)
+        return batch_response
 
     # Create groups in Entra ID
-    def create_groups(self,):
-    
+    #def create_groups(self,):
+
+if __name__ == '__main__':
+    AES_KEY = os.getenv("AES_KEY")
+    loadData = LoadEntraIdData(AES_KEY)
+    users_res = loadData.create_users("users.json.enc", "users_batch_res.json")
+    print(json.dumps(users_res, indent=2, ensure_ascii=False))
